4a5810b70c87edcc2c1614e29de177d6bf3d410a
sondac <you@example.comcls>
2020-04-10 21:18:50 +0200

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Cards from './components/Cards/Cards'
+import Chart from './components/Chart/Chart'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import styles from './App.module.css'
+import { fetchData } from './api/index'
+import Image1 from './Images/image.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    data: {},
+    country: ''
+  }
+
+  async componentDidMount () {
+    const fetchedData = await fetchData()
+    this.setState({data: fetchedData})   
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });  
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={Image1} alt=""/>
+        <Cards data={this.state.data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={this.state.data} country={this.state.country}/>
+      </div>
+    );
+  }
 }
-
-export default App;
